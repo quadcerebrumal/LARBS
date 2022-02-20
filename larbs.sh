97262e4f08eee579b8cb8ef8a69d48a4a76d99ac
Jenny Rakete <rakete@rakete.xyz>
2022-02-20 20:44:29 +0100

    fix name...
    i'm kinda retarded

--- larbs.sh
+++ larbs.sh
@@ -14,7 +14,7 @@ while getopts ":a:r:b:p:h" o; do case "${o}" in
 	*) printf "Invalid option: -%s\\n" "$OPTARG" && exit 1 ;;
 esac done
 
-[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/quadcrebrumal/voidrice.git"
+[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/quadcerebrumal/voidrice.git"
 [ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/quadcerebrumal/LARBS/master/progs.csv"
 [ -z "$aurhelper" ] && aurhelper="yay"
 [ -z "$repobranch" ] && repobranch="master"
